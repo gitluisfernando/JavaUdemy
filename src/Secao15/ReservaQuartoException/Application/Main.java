e68eaeccf5134da2ed63a14f8f4a4a28fb6d4989
Luis Fernando Ribeiro <mail.luisfernando@gmail.com>
2020-07-06 11:45:37 -0300

    Exercício da seção 15 - Excessão - Segunda solução

--- src/Secao15/ReservaQuartoException/Application/Main.java
+++ src/Secao15/ReservaQuartoException/Application/Main.java
@@ -1,4 +1,47 @@
 package Secao15.ReservaQuartoException.Application;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+import Secao15.ReservaQuartoException.Model.Entities.ReservaQuarto;
+
 public class Main {
+
+    public static void main(String[] args) throws ParseException {
+
+        Scanner entrada = new Scanner(System.in);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+        System.out.print("Número do Quarto: ");
+        Integer numeroQuarto = entrada.nextInt();
+        System.out.print("Data de CheckIn (dd/MM/aaaa): ");
+        Date dataCheckin = sdf.parse(entrada.next());
+        System.out.print("Data de CheckOut (dd/MM/aaaa): ");
+        Date dataCheckout = sdf.parse(entrada.next());
+
+        if (!dataCheckout.after(dataCheckin)) {
+            System.out.println("Erro na reserva: A data de Checkout deve ser superior a de Checkin");
+        } else {
+            ReservaQuarto reservaQuarto = new ReservaQuarto(numeroQuarto, dataCheckin, dataCheckout);
+            System.out.println("Reserva: " + reservaQuarto);
+
+            System.out.println();
+            System.out.println("Entre com os dados para atualizar a reserva: ");
+            System.out.print("Data de CheckIn (dd/MM/aaaa): ");
+            dataCheckin = sdf.parse(entrada.next());
+            System.out.print("Data de CheckOut (dd/MM/aaaa): ");
+            dataCheckout = sdf.parse(entrada.next());
+
+            String erro = reservaQuarto.atualizarDatas(dataCheckin, dataCheckout);
+            if (erro != null) {
+                System.out.println("Erro na reserva: " + erro);
+            } else {
+                System.out.println("Reserva: " + reservaQuarto);
+            }
+        }
+
+    }
+
 }
--- src/Secao15/ReservaQuartoException/Model/Entities/ReservaQuarto.java
+++ src/Secao15/ReservaQuartoException/Model/Entities/ReservaQuarto.java
@@ -1,4 +1,64 @@
 package Secao15.ReservaQuartoException.Model.Entities;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
 public class ReservaQuarto {
+
+    private Integer numeroQuarto;
+    private Date dataCheckin;
+    private Date dataCheckout;
+
+    private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+    public ReservaQuarto(Integer numeroQuarto, Date dataCheckin, Date dataCheckout) {
+        this.numeroQuarto = numeroQuarto;
+        this.dataCheckin = dataCheckin;
+        this.dataCheckout = dataCheckout;
+    }
+
+    public Integer getNumeroQuarto() {
+        return numeroQuarto;
+    }
+
+    public void setNumeroQuarto(Integer numeroQuarto) {
+        this.numeroQuarto = numeroQuarto;
+    }
+
+    public Date getDataCheckin() {
+        return dataCheckin;
+    }
+
+    public Date getDataCheckout() {
+        return dataCheckout;
+    }
+
+    public Long duracaoEstadia() {
+        long diferenca = dataCheckout.getTime() - dataCheckin.getTime(); //Retorna a diferença em Milisegundos
+        return TimeUnit.DAYS.convert(diferenca, TimeUnit.MILLISECONDS); //Transforma o resultado em dias
+    }
+
+    public String atualizarDatas(Date dataCheckin, Date dataCheckout) {
+        Date agora = new Date();
+        if (dataCheckin.before(agora) || dataCheckout.before(agora)) {
+            return "Erro na reserva: A data não pode ser inferior a data atual";
+        } else if (!dataCheckout.after(dataCheckin)) {
+            return "Erro na reserva: A data de Checkout deve ser superior a de Checkin";
+        }
+        this.dataCheckin = dataCheckin;
+        this.dataCheckout = dataCheckout;
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        return "Quarto " +
+                numeroQuarto +
+                ", Checkin: " + sdf.format(dataCheckin) +
+                ", Checkout: " + sdf.format(dataCheckout) +
+                ", " + duracaoEstadia() +
+                " noites.";
+    }
+
 }
