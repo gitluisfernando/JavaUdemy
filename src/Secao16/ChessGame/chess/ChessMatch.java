7f4f44db1a722a0529d8d361a760b53f73b8fcf4
Luis Fernando Ribeiro <mail.luisfernando@gmail.com>
2020-07-16 11:49:44 -0300

    Projeto Xadrez - Pinta os squares das posições possíveis de uma peça

--- src/Secao16/ChessGame/chess/ChessMatch.java
+++ src/Secao16/ChessGame/chess/ChessMatch.java
@@ -25,6 +25,12 @@ public class ChessMatch {
         return mat;
     }
 
+    public boolean[][] possibleMoves(ChessPosition sourcePosition) {
+        Position position = sourcePosition.toPosition();
+        validateSourcePosition(position);
+        return board.piece(position).possibleMoves();
+    }
+
     //Takes the piece out of its original position and places it in the destination position
     public ChessPiece performChessMove(ChessPosition sourcePosition, ChessPosition targetPosition) {
         Position source = sourcePosition.toPosition();
