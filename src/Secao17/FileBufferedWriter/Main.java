830c6869583f2d28ebefe7f54917378e50989b59
Luis Fernando Ribeiro <mail.luisfernando@gmail.com>
2020-07-25 12:57:36 -0300

    Trabalhando com Arquivos

---
+++ src/Secao17/FileBufferedWriter/Main.java
@@ -0,0 +1,24 @@
+package Secao17.FileBufferedWriter;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String[] lines = new String[] { "Good Morning", "Good Afternoon", "Good Night" };
+        String path = "/home/luis/Particular/Estudos/JavaUdemy/src/Secao17/FileBufferedWriter/out.txt";
+
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))) {
+            for (String line : lines) {
+                bw.write(line);
+                bw.newLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
---
+++ src/Secao17/FileScanner/Main.java
@@ -0,0 +1,27 @@
+package Secao17.FileScanner;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        File file = new File("/home/luis/Particular/Estudos/JavaUdemy/src/Secao17/FileScanner/in.txt");
+        Scanner sc = null;
+
+        try {
+            sc = new Scanner(file);
+            while (sc.hasNextLine()) {
+                System.out.println(sc.nextLine());
+            }
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        } finally {
+            if (sc != null) {
+                sc.close();
+            }
+        }
+    }
+
+}
